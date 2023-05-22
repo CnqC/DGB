1f5082204ccbac1ee6047075468c14eb67cc54d3
CnqC <130760728+CnqC@users.noreply.github.com>
2023-05-22 17:32:30 +0700

    Making Script for Setting DiaLog p1
    Making Script for Setting DiaLog p1

--- Assets/CnqC/DGB/Scripts/IcomponentChecking.cs
+++ Assets/CnqC/DGB/Scripts/IcomponentChecking.cs
@@ -5,7 +5,7 @@ using CnqC.DGB;
 
 // Vì ta sử dụng rất nhiều Script khác nhau , nên t sẽ tạo ra 1 interFace để sử dụng cho các Script đó
 
-public interface IcomponentChecking 
+public interface IcomponentChecking
 {
     bool IscomponentNull(); // kiêm tra các Coponent được ta gọi ra có null hay không
 }
--- Assets/CnqC/DGB/Scripts/UI/SettingDiaLog.cs
+++ Assets/CnqC/DGB/Scripts/UI/SettingDiaLog.cs
@@ -1,21 +1,50 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CnqC.DGB;
 using UnityEngine.UI;
 
 
-public class SettingDiaLog : MonoBehaviour
+public class SettingDiaLog : DiaLog, IcomponentChecking
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider musicSilder;
+    public Slider soundSilder;
+
+    private AudioControler m_auCtr; // l?y ra cái audiocontroler trên scene
+
+    public bool IscomponentNull()
     {
-        
+       return  m_auCtr == null || musicSilder == null || soundSilder ==null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Show(bool isShow)
     {
-        
+        base.Show(isShow);
+
+        m_auCtr = FindAnyObjectByType<AudioControler>();
+
+        if (IscomponentNull()) return;
+
+
+
+    }
+
+    public void OnMusicChange(float value) // value = giá trị của thằng slider
+    {
+        if (IscomponentNull()) return;
+
+        // cập nhập lại volume ở trong cái musicAus của thằng audioCOntroler 
+
+        m_auCtr.musicVol = value;
+        m_auCtr.musicAus.volume = value; 
+    }
+
+    public void OnSoundChange(float value)
+    {
+        if (IscomponentNull()) return;
+
+        m_auCtr.soundVol = value;
+        m_auCtr.soundAus.volume = value;
+
     }
 }
